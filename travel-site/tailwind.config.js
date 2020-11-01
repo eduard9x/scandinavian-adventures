b277cc3e766f2c7837f46ed4980a51d783de4cfd
Eduard Ghinea <eduard@limesodatech.com>
2020-11-01 21:34:35 +0200

    Enabled tailwind and scss.

---
+++ travel-site/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
