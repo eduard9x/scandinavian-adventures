52c92284a50711fa0868c29b83e99cc9c06e889a
Eduard Ghinea <eduard@limesodatech.com>
2020-11-03 17:42:04 +0200

    Added user feedback to book experience. Fixed featured items.

--- travel-site/src/components/book-button.tsx
+++ travel-site/src/components/book-button.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { navigate } from 'gatsby'
 
 const BookButton = ({id, title, variant, featured, full, callback}) => {
+  const [disabled, setDisabled] = useState(false);
+
+  const onClick = () => {
+    setDisabled(true);
+    const interval = setInterval(() => {
+      callback();
+      setDisabled(false);
+      clearInterval(interval);
+    }, 250);
+  }
+
   return (
     <>
-      <button className={`book-button p-5 ${full ? 'w-full' : ''}`}
-              onClick={() => callback ? callback() : navigate(`/details`, {
+      <button className={`book-button p-5 ${disabled ? 'disabled flex' : ''} ${full ? 'w-full' : ''}`}
+              onClick={() => callback ? onClick() : navigate(`/details`, {
                 state: {
                   pageId: id,
                   featured,
@@ -15,7 +26,16 @@ const BookButton = ({id, title, variant, featured, full, callback}) => {
               style={{
                 backgroundColor: variant,
                 color: variant === 'black' ? 'white' : 'black',
-              }}>
+              }}
+              disabled={disabled}
+      >
+        {
+          !disabled ? '' :
+          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        }
         {title}
       </button>
     </>
--- travel-site/src/components/featured.tsx
+++ travel-site/src/components/featured.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Destination } from '../models/destination'
 import BookButton from './book-button'
 import isMobile from '../utils/mobile'
 import ReactMarkdown from 'react-markdown'
@@ -11,19 +10,20 @@ const Featured = ({data, first}) => {
   return (
     <div className={`featured ${first ? 'featured-first' : 'featured-second'} w-full my-10`}>
       <div className='image-holder relative'>
-        <img style={{ width: '100%', height: 418 }} src={data.media[mobile ? 'small' : 'large'].url}/>
+        <img className='same-height' style={{ width: '100%' }} src={data.media[mobile ? 'small' : 'large'].url}/>
         <div className='absolute flex flex-col top-0 right-0 bottom-0 left-0 justify-end px-5 md:p-3'>
           <h2 className='featured-title pb-4 text-white md:p-4'>{data.title}</h2>
         </div>
       </div>
       <div className='pt-4 ml-5'>
         <ReactMarkdown children={ body } />
-
+      </div>
+      <div className='pt-4 ml-5 mt-auto'>
         <p className='featured-cost pb-4 text-black pt-8'>
           {`From ${data.price.currencyCode === 'GBP' ? '£' : ''}${data.price.value} ${data.price.unit}`}
         </p>
       </div>
-      <div className='mt-auto pt-5'>
+      <div className='pt-5 md:ml-5'>
         <BookButton id={data.id} featured={true}/>
       </div>
     </div>
